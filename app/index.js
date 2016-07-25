b2b47ecc5a098065d9b9246a59a16b2513728469
curcuz <czkzcz.cmc@gmail.com>
2016-07-25 23:14:47 +0200

    let the scan finish before connection attempt

--- app/index.js
+++ app/index.js
@@ -7,9 +7,6 @@
     const chalk = require('chalk');
     const sleep = require('sleep');
 
-    const targetPort = process.env.TARGET_PORT || 'ttyS0';
-    const targetBaudrate = process.env.TARGET_BAUDRATE || 57600
-
     console.log(chalk.cyan("scanning for serial devices..."));
     sleep.sleep(3);
     SerialPort.list(function(err, ports) {
@@ -25,29 +22,31 @@
             console.log(chalk.green(JSON.stringify(foundPort)));
         });
     });
-    sleep.sleep(10);
-    let port = new SerialPort('/dev/' + targetPort, {
-        baudRate: parseInt(targetBaudrate),
-        parser: SerialPort.parsers.readline('\n')
+
+    let port = new SerialPort('/dev/' + process.env.TARGET_PORT, {
+        baudRate: parseInt(process.env.TARGET_BAUDRATE),
+        parser: SerialPort.parsers.readline('\n'),
+        autoOpen: false
     });
 
-    port.on('open', function() {
-        console.log(chalk.green('connected to ', process.env.TARGET_PORT, 'with baudrate: ', process.env.TARGET_BAUDRATE));
-        sleep.sleep(3);
-        console.log(chalk.cyan('write attempt: ', process.env.TEST_CMD));
-        port.write(process.env.TEST_CMD, function(err) {
-            if (err) {
-                return console.log(chalk.red('Error on write: ', err.message));
-            }
-            console.log(chalk.green('message written'));
+    setTimeout(function() {
+        port.open(function() {
+            console.log(chalk.green('connected to ', process.env.TARGET_PORT, 'with baudrate: ', process.env.TARGET_BAUDRATE));
+            sleep.sleep(3);
+            console.log(chalk.cyan('write attempt: ', process.env.TEST_CMD));
+            port.write(process.env.TEST_CMD, function(err) {
+                if (err) {
+                    return console.log(chalk.red('Error on write: ', err.message));
+                }
+                console.log(chalk.green('message written'));
+            });
         });
-    });
+    }, 10000);
 
     port.on('data', function(data) {
         console.log(chalk.yellow('Data received: ', data, '\n'));
     })
 
-    // open errors will be emitted as an error event
     port.on('error', function(err) {
         console.log('Error: ', err.message);
     })
